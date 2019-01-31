72db9401c0d16365196bd12d6cc84b9bad56a73d
IuliiaCOLUSSI <45996466+IuliiaCOLUSSI@users.noreply.github.com>
2019-01-31 16:44:04 +0100

    js horloge + enrike tp

---
+++ JS/exo/js/04_exo4.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    setinterval(fSec, 1000);
+    var sec = 0;
+    function fSec()
+}
--- JS/exo/js/07_exo.js
+++ JS/exo/js/07_exo.js
@@ -34,15 +34,15 @@ if(a > b ){
 }
 
 
-if (isNan(a) && (!isNan(b))) {
-    if(a < b) {
-        document.write('Votre 1ere nombre: ' + a + 'est plus petite que ' + b)
-    } else if (a > b) {
-        document.write('le' + a + 'est plus grande que ' + b);
-         } else {
-        document.write('la' + a + 'est egale ' + b);
+// if (isNan(a) && (!isNan(b))) {
+//     if(a < b) {
+//         document.write('Votre 1ere nombre: ' + a + 'est plus petite que ' + b)
+//     } else if (a > b) {
+//         document.write('le' + a + 'est plus grande que ' + b);
+//          } else {
+//         document.write('la' + a + 'est egale ' + b);
 
-         }
-}else {
-    alert('Vous n\'avez pas saisi de chiffre');
-}
+//          }
+// }else {
+//     alert('Vous n\'avez pas saisi de chiffre');
+// }
