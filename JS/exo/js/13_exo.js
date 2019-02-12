934983d152f09ec3dcf0e3592b5f5390af3e7eac
IuliiaCOLUSSI <45996466+IuliiaCOLUSSI@users.noreply.github.com>
2019-02-12 16:51:57 +0100

    day of js tasks

---
+++ JS/exo/js/13_exo.js
@@ -0,0 +1,9 @@
+
+
+var j = prompt('Mettrez votre nombre svp');
+
+if ((j >= 50) && (j <= 100)){ 
+    document.write("Merci");
+}else {
+    document.write('Essayez autre chiffre')
+}
---
+++ JS/exo/js/14_parite.js
@@ -0,0 +1,31 @@
+
+
+var chiffre = 1;
+while (chiffre<=10) {
+    document.write(chiffre + " multiplie " + c + " egale " + (c * chiffre) + "<br/>");
+    chiffre++;
+}
+
+
+
+
+
+
+
+// while (var c = prompt("cest la quelle nombre alors?"); (c >= 2) && (c <= 9)
+
+// var temps = 0;
+// for (var mesSous = 1000; mesSous < 2000; mesSous += 50) {
+//     document.write('<h3>Le mois suivant jai :' + mesSous + '</h3>');
+//     temps++;
+// }
+// document.write('<h3> Il aura fallu' + temps + 'mois</h3>');
+
+
+
+// var j = 1;
+// var c = prompt('Combien des tours de manege vous voulez?');
+// while (j <= c) {
+//     alert('Cest le tour de manège n°' + j);
+//     j++;
+// }
